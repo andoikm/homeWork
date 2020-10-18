8f33eb6dad56bbd576a7f03df852ef233d1510f9
andranik.minasyan@betconstruct.com <andranik.minasyan@betconstruct.com>
2020-10-19 00:30:06 +0400

    code

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ const getSortType = (arr, i, sort) => {
 	const flag =  arr[i] - arr[i + 1];
 	
 	return sort === ASC
-		? flag < 0
-		: flag > 0;
+		? flag > 0
+		: flag < 0;
 }
 
 
@@ -17,7 +17,7 @@ const swappElements = (arr, i) => {
 	arr[i + 1] = temp;
 }
 
-const bubbleSort = (arr, sort = DESC) => {
+const bubbleSort = (arr, sort = ASC) => {
 	const len = arr.length;
 	let swapped;
 	
@@ -35,6 +35,39 @@ const bubbleSort = (arr, sort = DESC) => {
 }
 
 const arr = [1,3,2,6,5,4,8,9,15];
+windowInitialText.innerText = arr;
 
-console.log(bubbleSort(arr, ASC));
+const addActiveClass = element => {
+	const elements = document.getElementsByTagName('button');
+	const len = elements.length;
+	
+	for(let i = 0; i < len; ++i) {
+		elements[i].classList = '';
+	}
+	
+	if(element){
+		element.classList += 'active';
+	}
+}
+
+runBtnAsc.addEventListener('click', () => {
+	const result = bubbleSort(arr, ASC);
+	
+	addActiveClass(runBtnAsc);
+	windowText.innerText = result;
+});
+
+runBtnDesc.addEventListener('click', () => {
+	const result = bubbleSort(arr, DESC);
+	
+	addActiveClass(runBtnDesc);
+	windowText.innerText = result;
+});
+
+resetBtn.addEventListener('click', () => {
+	addActiveClass();
+	windowText.innerText = '';
+});
+
+
 
